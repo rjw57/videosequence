e997dc3bf1e9fc0d24f1117f86ac2a0d0295916e
Rich Wareham <rich@richwareham.com>
2016-04-01 22:31:32 +0100

    add PyPI metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
+import os
 from setuptools import setup, find_packages
+
+def read_file(path):
+    with open(os.path.join(os.path.dirname(__file__), path), "r") as fobj:
+        return fobj.read()
 
 setup(
     name="videosequence",
-    version="0.1.0",
-    author="Rich Wareham",
-    author_email="rich.videosequence@richwareham.com",
-
+    version="1.0.0",
     packages=find_packages(),
 
     install_requires=["future", "pillow"],
+
+    author="Rich Wareham",
+    author_email="rich.videosequence@richwareham.com",
+    url="https://github.com/rjw57/videosequence",
+    description="Read from and seek into video files as if they were Python "
+                "sequences of PIL.Image-s.",
+    long_description=read_file("README.rst"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Topic :: Multimedia :: Video",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    license="MIT",
 )
